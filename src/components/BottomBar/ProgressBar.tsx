22fa469ffbc9f6f8a8acda6e53ca4087a2a9f75b
Caleb Johnson <calebgaryj@gmail.com>
2026-07-08 13:33:45 -0700

    Completed UI of the bottom bar for music player by adding in time played and duration

--- src/components/BottomBar/ProgressBar.tsx
+++ src/components/BottomBar/ProgressBar.tsx
@@ -1,9 +1,21 @@
 export default function ProgressBar() {
     return (
-        <div className="w-360 h-2 bg-white rounded-full">
-            <div>
+        <>
+            {getTime()}
+            <div className="w-360 h-2 bg-white rounded-full">
+                <div>
 
+                </div>
             </div>
-        </div>
+        </>
+    )
+}
+
+function getTime() {
+    return (
+        <>
+            <p className="fixed left-52 bottom-11">0:00</p>
+            <p className="fixed right-52 bottom-11">0:00</p>
+        </>
     )
 }
